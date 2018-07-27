4fc91ec37935c0fd52e6e82a61fbeadfbd4290be
cub7090 <cub7090@gmail.com>
2018-07-28 03:08:13 +0530

    Ckeditor video embed

--- app/assets/javascripts/ckeditor/config.js
+++ app/assets/javascripts/ckeditor/config.js
@@ -2,5 +2,6 @@ CKEDITOR.editorConfig = function (config) {
   config.height = '30em';  
   config.filebrowserImageBrowseUrl = '/ckeditor/pictures';
   config.filebrowserImageUploadUrl = '/ckeditor/pictures';
+  config.extraAllowedContent = 'iframe[*]'
   config.allowedContent = true;
 }
